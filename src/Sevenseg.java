e5e6220f3aa015f278fe2a553ffb8ffd8b602657
MattS <mateusz.szmyjda@gmail.com>
2020-03-26 01:01:59 +0100

    wczytanie wzorca

--- src/Sevenseg.java
+++ src/Sevenseg.java
@@ -1,5 +1,25 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class Sevenseg extends JPanel {
-
+	
+	private File basicDigitPath = new File("7seg.png");{
+	
+	try {
+	BufferedImage basicDigit = ImageIO.read(basicDigitPath);
+	}
+	catch(FileNotFoundException e) {
+		System.out.println("Brak wzoru wyświetlacza 7segmentowego!");
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+			}
 }
+
