f4887d3379771ae84baf6bf83efbe58416cea489
MattS <mateusz.szmyjda@gmail.com>
2020-04-05 18:09:24 +0200

    dodanie tworzenia kolejnych cyfr

--- src/Sevenseg.java
+++ src/Sevenseg.java
@@ -69,22 +69,156 @@ public class Sevenseg extends JPanel {
 		return backgroundColor;
 	}
 	
-	public void displayDigit(int number)
+	public void createDigit(int number) throws IOException 
 	{
+		BufferedImage temp = ImageIO.read(basicDigitPath);
+		
+		try {
+			//ImageIO.write(temp, "png", new File("test.png"));
+			ImageIO.write(basicDigit, "png",new File("szkic.png"));
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
 		switch(number)
 		{
 		case 0:
+			
+			 //temp = basicDigit;
 			for(int i=0; i<94;i++)
 			{
 				for(int j=0;j<144;j++)
 				{
 					if(basicDigit.getRGB(i, j)==segAcolor || basicDigit.getRGB(i, j)==segBcolor || basicDigit.getRGB(i, j)==segCcolor|| basicDigit.getRGB(i, j)==segDcolor || basicDigit.getRGB(i, j)==segEcolor || basicDigit.getRGB(i, j)==segFcolor)
 					{
+						temp.setRGB(i, j, primaryColor.getRGB());
 						
-						
+					}
+					
+					else if(basicDigit.getRGB(i, j)==segGcolor)
+					{
+						temp.setRGB(i, j, secondaryColor.getRGB());
 					}
 				}
 			}
+			try {
+				ImageIO.write(temp, "png", new File(""+number+".png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			temp=basicDigit;
+			break;
+			
+		case 1:
+		
+		//	temp = basicDigit;
+			for(int i=0; i<94;i++)
+			{
+				for(int j=0;j<144;j++)
+				{
+					if( basicDigit.getRGB(i, j)==segBcolor || basicDigit.getRGB(i, j)==segCcolor )
+					{
+						temp.setRGB(i, j, primaryColor.getRGB());
+						
+					}
+					
+					else if(basicDigit.getRGB(i, j)==segAcolor|| basicDigit.getRGB(i, j)==segGcolor || basicDigit.getRGB(i, j)==segDcolor|| basicDigit.getRGB(i, j)==segEcolor || basicDigit.getRGB(i, j)==segFcolor)
+					{
+						temp.setRGB(i, j, secondaryColor.getRGB());
+					}
+				}
+			}
+			try {
+				ImageIO.write(temp, "png", new File(""+number+".png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			temp=basicDigit;
+			break;
+			
+		case 2:
+			
+			//temp = basicDigit;
+			for(int i=0; i<94;i++)
+			{
+				for(int j=0;j<144;j++)
+				{
+					if(basicDigit.getRGB(i, j)==segAcolor || basicDigit.getRGB(i, j)==segBcolor || basicDigit.getRGB(i, j)==segDcolor || basicDigit.getRGB(i, j)==segEcolor|| basicDigit.getRGB(i, j)==segGcolor )
+					{
+						temp.setRGB(i, j, primaryColor.getRGB());
+						
+					}
+					
+					else if(basicDigit.getRGB(i, j)==segCcolor || basicDigit.getRGB(i, j)==segFcolor)
+					{
+						temp.setRGB(i, j, secondaryColor.getRGB());
+					}
+				}
+			}
+			try {
+				ImageIO.write(temp, "png", new File(""+number+".png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+			
+			case 3:
+			
+			//temp = basicDigit;
+			for(int i=0; i<94;i++)
+			{
+				for(int j=0;j<144;j++)
+				{
+					if(basicDigit.getRGB(i, j)==segAcolor || basicDigit.getRGB(i, j)==segBcolor || basicDigit.getRGB(i, j)==segDcolor || basicDigit.getRGB(i, j)==segCcolor|| basicDigit.getRGB(i, j)==segGcolor )
+					{
+						temp.setRGB(i, j, primaryColor.getRGB());
+						
+					}
+					
+					else if(basicDigit.getRGB(i, j)==segEcolor || basicDigit.getRGB(i, j)==segFcolor)
+					{
+						temp.setRGB(i, j, secondaryColor.getRGB());
+					}
+				}
+			}
+			try {
+				ImageIO.write(temp, "png", new File(""+number+".png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+			
+			case 4:
+				
+				//temp = basicDigit;
+				for(int i=0; i<94;i++)
+				{
+					for(int j=0;j<144;j++)
+					{
+						if(basicDigit.getRGB(i, j)==segBcolor || basicDigit.getRGB(i, j)==segCcolor || basicDigit.getRGB(i, j)==segFcolor|| basicDigit.getRGB(i, j)==segGcolor )
+						{
+							temp.setRGB(i, j, primaryColor.getRGB());
+							
+						}
+						
+						else if(basicDigit.getRGB(i, j)==segAcolor || basicDigit.getRGB(i, j)==segEcolor || basicDigit.getRGB(i, j)==segDcolor)
+						{
+							temp.setRGB(i, j, secondaryColor.getRGB());
+						}
+					}
+				}
+				try {
+					ImageIO.write(temp, "png", new File(""+number+".png"));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
 		}
 	}
 }
